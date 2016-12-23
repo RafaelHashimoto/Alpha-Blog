07769711ad82a4b92f33ce1256ea45cd97d4e80e
hashimoto_wmd <hashimoto_wmd@hotmail.com>
2016-12-22 20:43:49 -0500

    user model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161219181407) do
+ActiveRecord::Schema.define(version: 20161223002637) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
