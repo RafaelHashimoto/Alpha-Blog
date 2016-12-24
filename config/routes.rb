950aaf77860f82a0f77ec9d0a4bc10e7123d177b
hashimoto_wmd <hashimoto_wmd@hotmail.com>
2016-12-23 21:02:53 -0500

    signup page created

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   
   root 'welcome#home'
   get 'about' => 'welcome#about'
-  
+  get 'signup' => 'users#new'
+  post 'users' =>  'users#create'
   resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
